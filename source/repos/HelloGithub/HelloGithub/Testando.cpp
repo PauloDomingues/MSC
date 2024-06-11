a8468dde0755cdb3624918152de61e19045667c2
Peaga <phddorneles@gmail.com>
2024-06-11 11:40:52 -0300

    aah

--- source/repos/HelloGithub/HelloGithub/Testando.cpp
+++ source/repos/HelloGithub/HelloGithub/Testando.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 int main() {
-	cout << "Ola Github";
+	int x ;
+	int y ;
+	cout << "Digite o Primeiro Valor";
+	cin >> x;
+	cout << "Digite o Segundo Valor";
+	cin >> y;
+	return x + y;
+	
 	
 }
